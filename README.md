c9962035d25ab65ec28ff052fbe1f9619a4da779
Boris Rasin <boris@pointx.org>
2020-03-29 00:53:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿Example project for [cmodule](https://github.com/scapix-com/cmodule) package manager:
+
+```bash
+git clone https://github.com/scapix-com/cmodule_test
+cd cmodule_test
+cmake -B build
+cmake --build build
+```
---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+#include <zlib.h>
+#include <bzlib.h>
+#include <pybind11/pybind11.h>
+
+int main()
+{
+//	auto r1 = gzwrite(0, 0, 0);
+//	auto r2 = BZ2_bzCompressInit(0, 0, 0, 0);
+
+//	boost::filesystem::path p("hello");
+
+	char c = 500;
+
+	return 0;
+}
